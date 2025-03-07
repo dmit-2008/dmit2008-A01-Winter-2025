2f9bfa008eec5f5309be340155505325ea82d0be
Daniel Mouris <dgmouris@gmail.com>
2025-03-07 09:05:12 -0700

    class progress pushed

---
+++ 12-react-rest-reviews-app/reviews-rest-app/utils/api/reviews.js
@@ -0,0 +1,8 @@
+// because the backend is on a different domain.
+// we make the domain separate because our domains
+// will be different for production, testing, local.
+const BASE_URL = "http://localhost:5000"
+
+// a function that's going to fetch with get
+// I want you to update the index.js to use function
+// export and use it in index.js
