11baeb48db06af68ecd40b62cf23d231702ae052
Daniel Mouris <dgmouris@gmail.com>
2025-03-19 14:22:05 -0600

    class progress pushed

--- 16-hackernews-with-lifecycle/hackernews-lifecycle/components/TopStoriesList.js
+++ 16-hackernews-with-lifecycle/hackernews-lifecycle/components/TopStoriesList.js
@@ -1,6 +1,9 @@
 export default function TopStoriesList() {
+  // state
+    // one for the entirety
+    // one for the "rendered list"
 
-
+  // an effect that loads the topstories to a list
 
   // return
     // a list of Story components that will have
