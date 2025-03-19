3f8002122f70b7ad47cd321f31f008e63fe202e5
Daniel Mouris <dgmouris@gmail.com>
2025-03-19 14:25:59 -0600

    class progress pushed

--- 16-hackernews-with-lifecycle/hackernews-lifecycle/components/TopStoriesList.js
+++ 16-hackernews-with-lifecycle/hackernews-lifecycle/components/TopStoriesList.js
@@ -1,3 +1,6 @@
+import Button from "@mui/material/Button"
+
+
 export default function TopStoriesList() {
   // state
     // one for the entirety
@@ -5,9 +8,13 @@ export default function TopStoriesList() {
 
   // an effect that loads the topstories to a list
 
-  // return
-    // a list of Story components that will have
-    // a prop of id.
+  return <>
+    {
+      // a list of Story components that will have
+      // a prop of id.
+    }
+    <Button variant="contained">Load five more stories</Button>
+  </>
     // a "load 5 more button"
 
 }
--- 16-hackernews-with-lifecycle/hackernews-lifecycle/pages/index.js
+++ 16-hackernews-with-lifecycle/hackernews-lifecycle/pages/index.js
@@ -1,6 +1,8 @@
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
+import TopStoriesList from '@/components/TopStoriesList';
+
 export default function Home() {
   return (
     <div>
@@ -8,6 +10,7 @@ export default function Home() {
         <Typography component="h1" variant="h5">
           Top Hacker News Stories
         </Typography>
+        <TopStoriesList />
       </Container>
     </div>
   );
