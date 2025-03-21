2e82d3b1c8f16e27eee30ce530bb0419c2ea121a
Daniel Mouris <dgmouris@gmail.com>
2025-03-21 08:17:34 -0600

    class progress pushed

--- 16-hackernews-with-lifecycle/hackernews-lifecycle/components/Story.js
+++ 16-hackernews-with-lifecycle/hackernews-lifecycle/components/Story.js
@@ -1,14 +1,26 @@
 // remember destructure the prop.
 export default function Story({id}) {
+  // I want you to create a stateful value
+  // for isLoading (defaults true)
+  // for story (which will hold the data)
+
   // we're going to make an effect
   // that is going to listen to the changes
   // in the id
+
+  // see if you can handle the loading state
+  // and print the title.
+
+
   // when the id is rendered
-  // this will fetch s0tory url
+  // this will fetch story url
   // from here https://github.com/HackerNews/API?tab=readme-ov-file#items
 
   // we're going to whip up some cards with some links
   // and wrap that up.
+
+
+
 
   return <p>
     {id}
