ffa134d3a4f3749b3ab6cd43cd2201312af40c6a
Daniel Mouris <dgmouris@gmail.com>
2025-03-28 09:13:03 -0600

    class progress pushed

--- 19-nextjs-pages-and-space/space-pages/pages/agency/[agencyId].js
+++ 19-nextjs-pages-and-space/space-pages/pages/agency/[agencyId].js
@@ -1,4 +1,6 @@
-import Head from 'next/head'
+// since we'll be using the path variables
+// need to import the router
+import {useRouter} from 'next/router'
 
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
@@ -7,12 +9,17 @@ import Box from '@mui/material/Box';
 import NavBar from '@components/NavBar';
 
 export default function Agency() {
+  // we're going initialize the router.
+  const router = useRouter()
+  // we're going to use the "agencyId" variable
+  // from the path because our filename is [agencyId].js
+  // which is a dynamic path
+
 
   return (
     <div>
       <NavBar />
       <Container sx={{paddingTop:2}} component="main" maxWidth="xs">
-
         <Typography variant="h3">
           Agency
         </Typography>
