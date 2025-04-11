b6e825350a5cc1442d536a893d41ec832243158b
Daniel Mouris <dgmouris@gmail.com>
2025-04-11 09:17:59 -0600

    class progress pushed

--- 23-next-js-testing-with-mocking-fetch/mounting-rest-fundamentals-example/utils/api/quotes.js
+++ 23-next-js-testing-with-mocking-fetch/mounting-rest-fundamentals-example/utils/api/quotes.js
@@ -1,12 +1,12 @@
 import {BASE_URL } from './base.js'
 
 const getRandomQuote = () => {
-  return fetch(`${BASE_URL}/random`)
+  return fetch(`${BASE_URL}/random_quote`)
     .then((response)=> {
       return response.json()
     }).then((data)=> {
       return Promise.resolve(data)
-    })    
+    })
 }
 
 export { getRandomQuote }
