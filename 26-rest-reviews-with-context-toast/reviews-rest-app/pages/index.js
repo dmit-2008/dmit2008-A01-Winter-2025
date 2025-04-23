cd34a41dc80154fd648278f566447f9f90bf7c05
Daniel Mouris <dgmouris@gmail.com>
2025-04-23 13:40:36 -0600

    class progress pushed

--- 26-rest-reviews-with-context-toast/reviews-rest-app/pages/index.js
+++ 26-rest-reviews-with-context-toast/reviews-rest-app/pages/index.js
@@ -1,4 +1,5 @@
-import {useState, useEffect } from 'react'
+// to use a context you'll need the useContext hook.
+import {useState, useEffect, useContext } from 'react'
 
 import Container from '@mui/material/Container';
 
@@ -7,9 +8,15 @@ import AdaptationReviewForm from '../components/AdaptationReviewForm';
 import NavBar from '../components/NavBar'
 import SEO from '../components/SEO'
 
+// let's import just the context
+import { AppNotificationContext } from '../components/state/AppNotification';
+
 import { getReviews } from '../utils/api/reviews'
 
 export default function Home() {
+  // we're going get the notification using the context
+  const notification = useContext(AppNotificationContext)
+
   const [reviews, setReviews] = useState([])
 
   useEffect(()=> {
