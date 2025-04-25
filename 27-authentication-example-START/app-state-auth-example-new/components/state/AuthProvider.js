466ec52aff9541f7fe5cc4811ed0a909a661d9f7
Daniel Mouris <dgmouris@gmail.com>
2025-04-25 08:22:27 -0600

    class progress pushed

--- 27-authentication-example-START/app-state-auth-example-new/components/state/AuthProvider.js
+++ 27-authentication-example-START/app-state-auth-example-new/components/state/AuthProvider.js
@@ -1,5 +1,8 @@
 // what we're going to do here.
 // 1. create the component and wrap the _app.js
+// 2. create a context with an empty object
+//    Note: dan uses an empty object so that he can pass multiple values
+//    down to the context from the component so that you can access more stateful pieces
 
 
 
