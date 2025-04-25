d4943294adfcb58ce382500000110d5cca16c2a6
Daniel Mouris <dgmouris@gmail.com>
2025-04-25 08:21:10 -0600

    class progress pushed

--- 27-authentication-example-START/app-state-auth-example-new/pages/_app.js
+++ 27-authentication-example-START/app-state-auth-example-new/pages/_app.js
@@ -1,10 +1,16 @@
 import '@/styles/globals.css'
 
 import AppNotification from '@/components/state/AppNotification'
+import AuthProvider from '@/components/state/AuthProvider'
 
 export default function App({ Component, pageProps }) {
   return <AppNotification>
-    <Component {...pageProps} />
+    {/* we're doing a similiar thing here to the AppNotification except we'll
+    make the user and associated auth data and functions available to all of the
+    components. */}
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
   </AppNotification>
 
 }
